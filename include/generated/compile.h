f27d2c0ca71b7f1995997f785e4675e0d67586c2
ISTweak <istweak@d-o-b.org>
2013-02-02 07:12:41 +0900

    cleancacheドライバ削除＆row-iosched追加

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 25 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#25 PREEMPT Fri Feb 1 23:50:57 JST 2013"
+#define LINUX_COMPILE_TIME "23:50:57"
+#define LINUX_COMPILE_BY "love"
+#define LINUX_COMPILE_HOST "marijuana"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.6.3 (Sourcery CodeBench Lite 2012.03-56) "
